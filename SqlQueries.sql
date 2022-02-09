ad65efd3b2c0057acf12ab60977d27d37df7566d
tanveer hussain <rajasahib@tanveers-MacBook-Pro.local>
2022-02-09 14:38:03 -0500

    sql queries

---
+++ SqlQueries.sql
@@ -0,0 +1,6 @@
+SELECT * FROM persons WHERE first name 'peter';
+LIST "last name" , "email" , "hire date" and "max salary" WHERE "max salary" > '9000' < '12000' 
+SELECT * FROM persons WHERE first name ='peter'; 
+SELECT * FROM perons WHERE  first name LIKE  'a%z'
+ from  salary  WHERE "max salary" > '9000' < '12000' 
+ COUNT * FROM salesman WHERE range > 0.12 < 0.15
